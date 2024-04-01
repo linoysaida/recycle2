cdf96ebb6bf1cc6e0bfed60ca24d3c98036d1d9f
Linoy <linoysaida99@gmail.com>
2024-04-01 14:20:07 +0300

    last version

--- app/src/main/java/com/example/myapplication/MapFunction.java
+++ app/src/main/java/com/example/myapplication/MapFunction.java
@@ -1,45 +1,38 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFunction extends AppCompatActivity {
+public class MapFunction extends AppCompatActivity  implements OnMapReadyCallback {
 
+    private GoogleMap myMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_fanction);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_map_function);
 
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-// מאזין ללחיצות על פריטים בתפריט התחתון
-        bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
-        // Populate the spinner with material options
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
-    private boolean onNavigationItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_home) { // החלף ב-ID המדויק של פריט "ראשי" שלך
-            // טען מחדש את HomePage או נווט אליו אם אתה נמצא בפעילות אחרת
-            Intent intent = new Intent(MapFunction.this, HomePage.class);
-            startActivity(intent);
-            return true;
-            // אפשר להוסיף כאן מקרים נוספים לפריטים אחרים בתפריט
-        }
 
-        if (item.getItemId() == R.id.action_logout) { // החלף ב-ID המדויק של פריט "ראשי" שלך
-            // טען מחדש את HomePage או נווט אליו אם אתה נמצא בפעילות אחרת
-            Intent intent = new Intent(MapFunction.this, LogIn.class);
-            startActivity(intent);
-            return true;
-            // אפשר להוסיף כאן מקרים נוספים לפריטים אחרים בתפריט
-        }
-        return false;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+
+        LatLng sharon_home = new LatLng(32.671273, 35.236130);
+        myMap.addMarker(new MarkerOptions().position(sharon_home).title("Marker in sharon_home"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sharon_home));
     }
 }
